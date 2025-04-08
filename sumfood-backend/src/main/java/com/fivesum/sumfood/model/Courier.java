2f2e3e7a5a78192ef0170870cdcc88161c23679d
Yusuf İpek <yusuf55ipek55@gmail.com>
2025-04-09 02:03:50 +0300

    courier table added

--- sumfood-backend/src/main/java/com/fivesum/sumfood/model/Courier.java
+++ sumfood-backend/src/main/java/com/fivesum/sumfood/model/Courier.java
@@ -2,14 +2,17 @@ package com.fivesum.sumfood.model;
 
 import java.util.Date;
 
-import javax.persistence.Column;
-import javax.persistence.EnumType;
-import javax.persistence.Enumerated;
-import javax.persistence.PrePersist;
+import javax.persistence.*;
+
 import com.fivesum.sumfood.model.base.UserBase;
 import com.fivesum.sumfood.model.enums.Role;
 import com.fivesum.sumfood.model.enums.VehicleType;
 
+import lombok.experimental.SuperBuilder;
+
+@SuperBuilder
+@Entity
+@Table(name = "couriers")
 public class Courier extends UserBase {
 
     @Column(nullable = false)
