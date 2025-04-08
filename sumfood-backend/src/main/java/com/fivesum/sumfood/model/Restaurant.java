10e451f01be0763e62cfabf50105ba0edb7b972c
Yusuf İpek <yusuf55ipek55@gmail.com>
2025-04-09 02:14:22 +0300

    restaurant model added (without location)

---
+++ sumfood-backend/src/main/java/com/fivesum/sumfood/model/Restaurant.java
@@ -0,0 +1,34 @@
+package com.fivesum.sumfood.model;
+
+import javax.persistence.*;
+
+import com.fivesum.sumfood.model.base.UserBase;
+import com.fivesum.sumfood.model.enums.Role;
+
+import lombok.experimental.SuperBuilder;
+
+@SuperBuilder
+@Entity
+@Table(name = "restaurants")
+public class Restaurant extends UserBase {
+    // User base fields is for Restaurant admin user
+
+    @Column(nullable = false, unique = true)
+    private String taxIdentificationNumber; // Vergi Kimlik Numarası
+
+    @Column(nullable = false, unique = true)
+    private String bussinesName; // Real Name
+
+    @Column(nullable = false, unique = true, length = 50)
+    private String displayName;
+
+    @Column(nullable = false)
+    private String description;
+
+    // TODO: Add location fields
+
+    @PrePersist
+    public void prePersist() {
+        setRole(Role.RESTAURANT);
+    }
+}
