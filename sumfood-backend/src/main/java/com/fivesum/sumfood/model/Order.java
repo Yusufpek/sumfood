5f8a13568aa3645cbe966ca5256bcdc1203b3a77
Yusuf İpek <yusuf55ipek55@gmail.com>
2025-04-09 02:36:07 +0300

    order model added

---
+++ sumfood-backend/src/main/java/com/fivesum/sumfood/model/Order.java
@@ -0,0 +1,27 @@
+package com.fivesum.sumfood.model;
+
+import javax.persistence.*;
+
+import com.fivesum.sumfood.model.base.EntityBase;
+import com.fivesum.sumfood.model.enums.OrderStatus;
+import com.fivesum.sumfood.model.enums.PaymentStatus;
+
+public class Order extends EntityBase {
+
+    @ManyToOne(optional = false)
+    @JoinColumn(name = "customer_id", referencedColumnName = "id")
+    private Customer customer;
+
+    @OneToOne
+    @JoinColumn(name = "shopping_cart_id", referencedColumnName = "id", unique = true)
+    private ShoppingCart shoppingCart;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private PaymentStatus paymentStatus;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private OrderStatus orderStatus;
+
+}
---
+++ sumfood-backend/src/main/java/com/fivesum/sumfood/model/enums/PaymentStatus.java
@@ -0,0 +1,7 @@
+package com.fivesum.sumfood.model.enums;
+
+public enum PaymentStatus {
+    WAITING,
+    SUCCESSFUL,
+    FAILED,
+}
