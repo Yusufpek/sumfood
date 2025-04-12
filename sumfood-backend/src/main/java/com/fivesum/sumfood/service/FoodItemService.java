1e48fabb249021a7e955384af7fe19808b272b90
Yusuf İpek <yusuf55ipek55@gmail.com>
2025-04-12 23:58:20 +0300

    add: add food item func

--- sumfood-backend/src/main/java/com/fivesum/sumfood/service/FoodItemService.java
+++ sumfood-backend/src/main/java/com/fivesum/sumfood/service/FoodItemService.java
@@ -2,9 +2,13 @@ package com.fivesum.sumfood.service;
 
 import java.util.List;
 
+import javax.transaction.Transactional;
+
 import org.springframework.stereotype.Service;
 
+import com.fivesum.sumfood.dto.FoodItemAddRequest;
 import com.fivesum.sumfood.model.FoodItem;
+import com.fivesum.sumfood.model.Restaurant;
 import com.fivesum.sumfood.model.enums.Category;
 import com.fivesum.sumfood.repository.FoodItemRepository;
 
@@ -22,6 +26,19 @@ public class FoodItemService {
 
     public List<FoodItem> getItemsByCategory(Category category) {
         return foodItemRepository.findByCategoriesContaining(category);
+    }
+
+    @Transactional
+    public FoodItem addFoodItem(FoodItemAddRequest request, Restaurant restaurant) {
+        FoodItem foodItem = FoodItem.builder()
+                .name(request.getName())
+                .description(request.getDescription())
+                .price(request.getPrice())
+                .stock(request.getStock())
+                .restaurant(restaurant)
+                .build();
+
+        return foodItemRepository.save(foodItem);
     }
 
 }
