7b2662f60ae4136c77e8d10fcb678d7b45f0b9e1
Yusuf İpek <yusuf55ipek55@gmail.com>
2025-04-12 04:29:25 +0300

    add: data decorator for getter and setters

--- sumfood-backend/src/main/java/com/fivesum/sumfood/responses/LoginResponse.java
+++ sumfood-backend/src/main/java/com/fivesum/sumfood/responses/LoginResponse.java
@@ -1,21 +1,14 @@
 package com.fivesum.sumfood.responses;
 
+import lombok.Data;
+
+@Data
 public class LoginResponse {
+    public LoginResponse(String token) {
+        this.token = token;
+    }
+
     private String token;
 
     private long expiresIn;
-
-    public String getToken() {
-        return token;
-    }
-
-    public void setToken(String token) {
-        this.token = token;
-    }
-    public long getExpiresIn() {
-        return expiresIn;
-    }
-    public void setExpiresIn(long expiresIn) {
-        this.expiresIn = expiresIn;
-    }
 }
