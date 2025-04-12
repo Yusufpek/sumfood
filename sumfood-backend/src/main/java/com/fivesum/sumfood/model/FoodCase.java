7566c88abb994edb3e6e24e78264bd2497860d33
Yusuf İpek <yusuf55ipek55@gmail.com>
2025-04-12 21:14:45 +0300

    update: category, menu relations

--- sumfood-backend/src/main/java/com/fivesum/sumfood/model/FoodCase.java
+++ sumfood-backend/src/main/java/com/fivesum/sumfood/model/FoodCase.java
@@ -5,13 +5,14 @@ import java.util.List;
 import javax.persistence.*;
 
 import com.fivesum.sumfood.model.base.EntityBase;
+import com.fivesum.sumfood.model.enums.Category;
 
 @Entity
 @Table(name = "food_cases")
 public class FoodCase extends EntityBase {
-    @OneToOne(optional = false)
-    @JoinColumn(name = "category_id", nullable = false)
-    private Category category;
+    @ManyToOne(optional = false)
+    @JoinColumn(name = "restaurant_id", nullable = false)
+    private Restaurant restaurant;
 
     @Column(nullable = false, length = 50)
     String title;
@@ -21,6 +22,12 @@ public class FoodCase extends EntityBase {
 
     @Column(nullable = false)
     double price;
+
+    @ElementCollection(targetClass = Category.class)
+    @Enumerated(EnumType.STRING)
+    @CollectionTable(name = "food_case_categories", joinColumns = @JoinColumn(name = "food_case_id"))
+    @Column(name = "category")
+    private List<Category> categories;
 
     // Relation
     @OneToMany(mappedBy = "foodCase", cascade = CascadeType.ALL, orphanRemoval = true, fetch = FetchType.LAZY)
