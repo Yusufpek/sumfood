ac310ecd8bf1d06d9ce1f626d0b900147c9aa9bb
Yusuf İpek <yusuf55ipek55@gmail.com>
2025-04-23 23:54:29 +0300

    add: image path constants

---
+++ sumfood-backend/src/main/java/com/fivesum/sumfood/constants/ImagePath.java
@@ -0,0 +1,15 @@
+package com.fivesum.sumfood.constants;
+
+public class ImagePath {
+    static final String IMAGE_BASE = "src/main/resources/static/images/";
+
+    // Food Item
+    public static final String FOOD_ITEM_PATH = IMAGE_BASE + "food_items/";
+
+    public static String getFootItemImagePathByRestaurant(String restaurantName) {
+        restaurantName = restaurantName.replace(" ", "_");
+        return FOOD_ITEM_PATH + restaurantName + "/";
+    }
+
+    public static final String DEFAULT_FOOD_ITEM_PATH = FOOD_ITEM_PATH + "default.png";
+}
