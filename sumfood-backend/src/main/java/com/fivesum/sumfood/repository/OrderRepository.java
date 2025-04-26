5154691eddcd91f38973fcd6648e5f29192da770
Yusuf İpek <yusuf55ipek55@gmail.com>
2025-04-26 19:33:40 +0300

    delete: order files

--- sumfood-backend/src/main/java/com/fivesum/sumfood/repository/OrderRepository.java
+++
@@ -1,9 +0,0 @@
-package com.fivesum.sumfood.repository;
-
-import com.fivesum.sumfood.model.Order;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface OrderRepository extends JpaRepository<Order, Long> {
-}
