5692d20c1d6abd2bcd53328916e175022c1a763c
b2210356037 <b2210356037@cs.hacettepe.edu.tr>
2025-04-26 18:30:09 +0300

    refactor: Clean up unnecessary comments

--- sumfood-backend/src/main/java/com/fivesum/sumfood/exception/ResourceNotFoundException.java
+++ sumfood-backend/src/main/java/com/fivesum/sumfood/exception/ResourceNotFoundException.java
@@ -1,9 +1,8 @@
-// src/main/java/com/fivesum/sumfood/exception/ResourceNotFoundException.java
 package com.fivesum.sumfood.exception;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-@ResponseStatus(HttpStatus.NOT_FOUND) // Helps Spring map this to 404 if uncaught
+@ResponseStatus(HttpStatus.NOT_FOUND)
 public class ResourceNotFoundException extends RuntimeException {
     public ResourceNotFoundException(String message) {
         super(message);
