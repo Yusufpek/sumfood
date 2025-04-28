5abf733ce9d2965ab91e1804b3df9cedf2faaea1
Yusuf İpek <yusuf55ipek55@gmail.com>
2025-04-28 19:40:47 +0300

    add: default address funcs to repository

--- sumfood-backend/src/main/java/com/fivesum/sumfood/repository/AddressRepository.java
+++ sumfood-backend/src/main/java/com/fivesum/sumfood/repository/AddressRepository.java
@@ -3,6 +3,9 @@ package com.fivesum.sumfood.repository;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.fivesum.sumfood.model.Address;
@@ -10,5 +13,12 @@ import com.fivesum.sumfood.model.Address;
 @Repository
 public interface AddressRepository extends JpaRepository<Address, Long> {
     void deleteById(Long id);
+
     Optional<Address> findById(Long id);
+
+    @Modifying
+    @Query("UPDATE Address a SET a.isDefault = false WHERE a.customer.id = :customerId")
+    void updateDefaultAddressFalse(@Param("customerId") Long customerId);
+
+    Optional<Address> getDefaultAddressByCustomerId(Long customerId);
 }
