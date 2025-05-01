26755e21f1b40d01fe6b1ef712b00710008b59d9
b2210356037 <b2210356037@cs.hacettepe.edu.tr>
2025-05-01 15:28:55 +0300

    feat: add routing for restaurant profile

--- sumfood-frontend/app/src/App.js
+++ sumfood-frontend/app/src/App.js
@@ -14,6 +14,7 @@ import './styles/global.css';
 import './styles/auth.css';
 import Profile from "./pages/Profile/Profile";
 import CreateOrderPage from './pages/CreateOrderPage/CreateOrderPage';
+import RestaurantPublicPage from './pages/RestaurantPublicPage/RestaurantPublicPage';
 
 function App() {
   return (
@@ -31,6 +32,9 @@ function App() {
                 </ProtectedRoute>
               }
             />
+            
+            <Route path="/restaurant/:restaurantId" element={<RestaurantPublicPage />} />
+
             <Route
               path="/restaurant-dashboard"
               element={
