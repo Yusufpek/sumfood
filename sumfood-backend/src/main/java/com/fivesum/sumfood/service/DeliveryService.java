f3a82b7ada9b0746ea8fc5119a98c9f3d00695cf
Yusuf İpek <yusuf55ipek55@gmail.com>
2025-05-01 13:57:02 +0300

    add: delivery service - find by order id

---
+++ sumfood-backend/src/main/java/com/fivesum/sumfood/service/DeliveryService.java
@@ -0,0 +1,19 @@
+package com.fivesum.sumfood.service;
+
+import org.springframework.stereotype.Service;
+
+import com.fivesum.sumfood.model.Delivery;
+import com.fivesum.sumfood.repository.DeliveryRepository;
+
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+@Service
+public class DeliveryService {
+    private DeliveryRepository deliveryRepository;
+
+    public Delivery findByOrderId(Long orderId) {
+        return deliveryRepository.findByOrderId(orderId).orElse(null);
+    }
+
+}
