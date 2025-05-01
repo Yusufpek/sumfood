28f2facc1f0a9b74ec7fe4537625aa9b3c46b0f9
b2210356037 <b2210356037@cs.hacettepe.edu.tr>
2025-05-01 19:31:43 +0300

    fix(review controller): add missing requestbody

--- sumfood-backend/src/main/java/com/fivesum/sumfood/controller/ReviewController.java
+++ sumfood-backend/src/main/java/com/fivesum/sumfood/controller/ReviewController.java
@@ -7,6 +7,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -29,7 +30,7 @@ public class ReviewController {
     private final JwtService jwtService;
 
     @PostMapping("/order/{orderId}")
-    public ResponseEntity<?> addOrder(@RequestHeader("Authorization") String token, ReviewRequest request,
+    public ResponseEntity<?> addOrder(@RequestHeader("Authorization") String token, @RequestBody ReviewRequest request,
             @PathVariable() String orderId) {
         String email = jwtService.extractUsername(token.replace("Bearer ", ""));
 
