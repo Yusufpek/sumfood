be9a1d8b5dddff9c4228d06bea04a14e148db02c
Yusuf İpek <yusuf55ipek55@gmail.com>
2025-05-02 04:29:39 +0300

    add: from location informations

--- sumfood-backend/src/main/java/com/fivesum/sumfood/service/DeliveryService.java
+++ sumfood-backend/src/main/java/com/fivesum/sumfood/service/DeliveryService.java
@@ -6,9 +6,11 @@ import java.util.stream.Collectors;
 import org.springframework.stereotype.Service;
 
 import com.fivesum.sumfood.dto.DeliveryResponse;
+import com.fivesum.sumfood.exception.ConflictException;
 import com.fivesum.sumfood.model.Courier;
 import com.fivesum.sumfood.model.Delivery;
 import com.fivesum.sumfood.model.Order;
+import com.fivesum.sumfood.model.Restaurant;
 import com.fivesum.sumfood.model.enums.OrderStatus;
 import com.fivesum.sumfood.repository.DeliveryRepository;
 
@@ -29,16 +31,23 @@ public class DeliveryService {
     }
 
     public DeliveryResponse toDeliveryResponse(Delivery delivery) {
+        Restaurant restaurant = delivery.getOrder().getShoppingCart().getRestaurant();
         return DeliveryResponse.builder()
                 .id(delivery.getId())
                 .createdAt(delivery.getCreateAt())
                 .order(orderService.toResponseDTO(delivery.getOrder()))
+                .fromAddress(restaurant.getAddress())
+                .fromLat(restaurant.getLatitude())
+                .fromLong(restaurant.getLongitude())
                 .courierName(delivery.getCourier().getName())
                 .courierVehicleType(delivery.getCourier().getVehicleType())
                 .build();
     }
 
     public DeliveryResponse createDelivery(Order order, Courier courier) {
+        if (deliveryRepository.existsByCourierIdAndOrder_OrderStatus(courier.getId(), OrderStatus.ON_THE_WAY)) {
+            throw new ConflictException("Courier has a on the way order");
+        }
         Delivery delivery = Delivery.builder()
                 .order(order)
                 .courier(courier)
@@ -57,5 +66,14 @@ public class DeliveryService {
     public List<DeliveryResponse> getDeliveriesByCourier(Courier courier) {
         List<Delivery> deliveries = deliveryRepository.findByCourierId(courier.getId());
         return deliveries.stream().map(delivery -> toDeliveryResponse(delivery)).collect(Collectors.toList());
+    }
+
+    public DeliveryResponse getActiveDeliveryByCourier(Courier courier) {
+        List<Delivery> deliveries = deliveryRepository.findByCourierIdAndOrder_OrderStatus(courier.getId(),
+                OrderStatus.ON_THE_WAY);
+        if (deliveries.size() > 0) {
+            return toDeliveryResponse(deliveries.get(0));
+        }
+        return null;
     }
 }
