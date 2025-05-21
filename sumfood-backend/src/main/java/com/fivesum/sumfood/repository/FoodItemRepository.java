edc9facd607b85b1aca2af6d9f308beca7e7f959
Yusuf İpek <yusuf55ipek55@gmail.com>
2025-05-21 22:49:39 +0300

    add: donated query

--- sumfood-backend/src/main/java/com/fivesum/sumfood/repository/FoodItemRepository.java
+++ sumfood-backend/src/main/java/com/fivesum/sumfood/repository/FoodItemRepository.java
@@ -1,6 +1,7 @@
 package com.fivesum.sumfood.repository;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
@@ -14,5 +15,7 @@ public interface FoodItemRepository extends JpaRepository<FoodItem, Long> {
     List<FoodItem> findByCategoriesContaining(Category category);
 
     List<FoodItem> findByRestaurant(Restaurant restaurant);
+
+    Optional<FoodItem> findByIsDonatedAndImageName(boolean isDonated, String imageName);
 
 }
